1099e3cb90141b9747b2f15fe72bb7771ccebed4
davidmihai3002 <david.popovici@thinslices.com>
2025-09-23 11:07:50 +0300

    added boilerplate header and footer

--- components/ui/app-sidebar.tsx
+++ components/ui/app-sidebar.tsx
@@ -1,15 +1,19 @@
-import { Bell, Calendar, ClipboardList, ClipboardPenLine, Home, Inbox, Search, Settings, Store, WalletMinimal } from "lucide-react"
+import { Bell, Calendar, ClipboardList, ClipboardPenLine, Home, Inbox, Search, Settings, Store, SunMoon, WalletMinimal } from "lucide-react"
 
 import {
   Sidebar,
   SidebarContent,
+  SidebarFooter,
   SidebarGroup,
   SidebarGroupContent,
   SidebarGroupLabel,
+  SidebarHeader,
   SidebarMenu,
+  SidebarMenuBadge,
   SidebarMenuButton,
   SidebarMenuItem,
 } from "@/components/ui/sidebar"
+import { Button } from "./button"
 
 // Menu items.
 const items = [
@@ -17,35 +21,43 @@ const items = [
     title: "Updates",
     url: "#",
     icon: Bell,
+    badge: 3,
   },
   {
     title: "Tasks",
     url: "#",
     icon: ClipboardList,
+    badge: 0,
   },
   {
     title: "Transactions",
     url: "#",
     icon: ClipboardPenLine,
+    badge: 1,
   },
   {
     title: "Vendors",
     url: "#",
     icon: Store,
+    badge: 5,
   },
   {
     title: "Wallets",
     url: "#",
     icon: WalletMinimal,
+    badge: 0,
   },
 ]
 
 export function AppSidebar() {
   return (
     <Sidebar collapsible="icon" side="left">
+      <SidebarHeader className="text-2xl font-bold text-red-500">
+        Amatsu
+      </SidebarHeader>
       <SidebarContent>
         <SidebarGroup>
-          <SidebarGroupLabel>Application</SidebarGroupLabel>
+          {/* <SidebarGroupLabel>Application</SidebarGroupLabel> */}
           <SidebarGroupContent>
             <SidebarMenu>
               {items.map((item) => (
@@ -56,12 +68,19 @@ export function AppSidebar() {
                       <span>{item.title}</span>
                     </a>
                   </SidebarMenuButton>
+                  <SidebarMenuBadge className="text-red-600">{item.badge > 0 ? item.badge : ""}</SidebarMenuBadge>
                 </SidebarMenuItem>
               ))}
             </SidebarMenu>
           </SidebarGroupContent>
         </SidebarGroup>
       </SidebarContent>
+      <SidebarFooter>
+        <Button variant={"ghost"}>
+          <SunMoon />
+          Change Theme
+        </Button>
+      </SidebarFooter>
     </Sidebar>
   )
 }
